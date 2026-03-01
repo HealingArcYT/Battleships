909618f912a3f13d5fbe3fe3e6bd986d5ab8ec79
ZAWADl <molld807@gmail.com>
2026-03-01 19:23:10 +0100

    Added player.py
    Player initializes its board
    Player can shoot onto the board if it is his turn

---
+++ player.py
@@ -0,0 +1,19 @@
+import board as b
+
+
+class Player:
+    canShoot: bool
+    board: board.Board
+
+    def __init__(self):
+        self.board = b.Board(self)
+
+    def shoot(self, x: int, y: int) -> bool:
+        if not self.canShoot:
+            return False
+
+        if self.board.board[x][y].shot:
+            return False
+
+        self.board.board[x][y].shot = True
+        return True
